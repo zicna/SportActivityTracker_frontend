e98ef0777dd5215928415c03ea76c4449044d25b
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-05 12:15:18 -0600

    add new component and declare routes in layout component

---
+++ src/components/Layout.js
@@ -0,0 +1,25 @@
+// * components
+import Welcome from './Welcome'
+import About from './About'
+import UserForm from "./UserForm";
+
+import { Route } from 'react-router-dom'
+
+const Layout = () => {
+    return(
+        <div>
+            <Route path='/welcome'>
+                <Welcome />
+            </Route>
+            <Route path='/about'>
+                <About />
+            </Route>
+            <Route path='/signin'>
+                <UserForm />
+            </Route>
+        </div>
+
+    )
+}
+
+export default Layout
