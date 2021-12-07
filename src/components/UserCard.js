f6732db7d11187e8fcaeea779104dd93587a655e
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-07 09:58:04 -0600

    buildup of UserCard

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -3,7 +3,16 @@ import React, { Component } from 'react'
 class UserCard extends React.Component{
     render(){
         return(
-            <h1>Hello User One {this.props.user.first_name}</h1>
+            <div>
+                <h1>Hello User {this.props.user.first_name}</h1>
+                <img src={this.props.user.image} alt="" />
+                <p> Start Weight: {this.props.user.start_weight}</p>
+                <p>Height: {this.props.user.height}</p>
+                <p>Gender: {this.props.user.gender}</p>
+                <p>Birth Date: {this.props.user.birth_date}</p>
+
+            </div>
+
         )
     }
 }
